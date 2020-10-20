47a0d7af3f1ad2ba093e683ad56b088c43d44666
sachsd <sachsd1116@outlook.com>
2020-10-20 09:14:14 -0400

    Index Slideshow added

--- scripts.js
+++ scripts.js
@@ -11,6 +11,12 @@ $(function() {
     });
 });
 
+$( document ).ready(function(){
+  $('.carousel').carousel({
+    interval: 4000
+  })
+});
+
 // $('.navbar-nav li').click(function(){
 //   $('.navbar-nav li').removeClass('active');
 //   $(this).addClass('active');
@@ -25,35 +31,3 @@ function openModal() {
   function closeModal() {
     document.getElementById("myModal").style.display = "none";
   }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var altText = document.getElementsByClassName("img-fluid hover-shadow");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    // for (i = 0; i < dots.length; i++) {
-    //   dots[i].className = dots[i].className.replace(" active", "");
-    // }
-    slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
-    captionText.innerHTML = altText[slideIndex-1].alt;
-  }
-  

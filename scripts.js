c713cc39b22f40de8972014654621bc5decc80ff
sachsd <sachsd1116@outlook.com>
2020-10-23 10:54:33 -0400

    Fixed issues that never should have existed in the first place

--- scripts.js
+++ scripts.js
@@ -22,6 +22,7 @@ $( document ).ready(function(){
 //   $(this).addClass('active');
 // })
 
+
 // Open the Modal
 function openModal() {
     document.getElementById("myModal").style.display = "block";
@@ -30,4 +31,28 @@ function openModal() {
   // Close the Modal
   function closeModal() {
     document.getElementById("myModal").style.display = "none";
+  }
+
+  var slideIndex = 1;
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var altText = document.getElementsByClassName("img-fluid hover-shadow");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //   dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    slides[slideIndex-1].style.display = "block";
+    // dots[slideIndex-1].className += " active";
+    captionText.innerHTML = altText[slideIndex-1].alt;
   }

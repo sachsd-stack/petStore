993491d3c6c937415fe0cd7595b4e60cd4d0b879
sachsd <sachsd1116@outlook.com>
2020-11-02 10:03:33 -0500

    Added Gallery and Modal For Loop

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(function() {
     $(".filter").click(function()   {
         selectedClass = $(this).attr("data-rel");
         $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+        $("#gallery .pics").not("."+selectedClass).fadeOut().removeClass('animation');
         setTimeout(function() {
           $("."+selectedClass).fadeIn().addClass('animation');
           $("#gallery").fadeTo(300, 1);
